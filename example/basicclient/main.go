620195c8ab9d591aa438a6d1b4f05e9c642eb742
Maxime Roussin-Belanger <maxime.roussinbelanger@gmail.com>
2017-10-19 01:19:41 -0400

    Add minimalist example of basic client creation

---
+++ example/basicclient/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/lorac/plex-go/plex"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	fmt.Print("Plex Username: ")
+	username, _ := r.ReadString('\n')
+
+	fmt.Print("Plex Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+	password := string(bytePassword)
+
+	tp := plex.PlexAuthTransport{}
+
+	token, err := plex.RequestPlexToken(username, password, tp.Client())
+	if err != nil {
+		fmt.Println("Unable to fetch a token")
+	}
+
+	tp.XPlexToken = token
+
+	client := plex.NewClient(tp.Client())
+	client.NewRequest("POST", "", nil)
+}
